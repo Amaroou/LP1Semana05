7071ba399b83cbb272886041d5426e4d39cea277
João Amaro <Jamaro2110@gmail.com>
2023-03-31 11:56:57 +0100

    Exercicio 7, pedir ao utilizador um numero e lançar esse numero de dados

---
+++ RandomDice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos dados quer lançar?");
+            int numerodados = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(numerodados);
+            int dicenum;
+            Random random = new Random();
+            int soma=0;
+            for (int i = 0; i < numerodados; i++)
+            {
+                dicenum = random.Next(1, 7);
+                Console.WriteLine("calhou: " + dicenum);
+
+            }
+
+
+
+
+        }
+    }
+}
